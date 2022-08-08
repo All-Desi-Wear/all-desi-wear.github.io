3873cda5927e191aadfbe6349fd45ecf177c4c90
Hemang Rajyaguru <hemdagem@gmail.com>
2022-08-08 18:34:42 +0100

    added search index

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -23,6 +23,66 @@ const config: GatsbyConfig = {
       path: `./src/data/`,
     },
   },
+  {
+    resolve: 'gatsby-plugin-local-search',
+    options: {
+      // A unique name for the search index. This should be descriptive of
+      // what the index contains. This is required.
+      name: 'pages',
+
+      // Set the search engine to create the index. This is required.
+      // The following engines are supported: flexsearch, lunr
+      engine: 'flexsearch',
+
+      // Provide options to the engine. This is optional and only recommended
+      // for advanced users.
+      //
+      // Note: Only the flexsearch engine supports options.
+      engineOptions: 'speed',
+
+      // GraphQL query used to fetch all data for the search index. This is
+      // required.
+      query: `
+        {
+          allDataJson {
+            nodes {
+              Brand
+              Description
+              Images
+              Link
+              Name
+              Price
+            }
+          }      
+        }
+      `,
+
+      // Field used as the reference value for each document.
+      // Default: 'id'.
+      ref: 'Name',
+
+      // List of keys to index. The values of the keys are taken from the
+      // normalizer function below.
+      // Default: all fields
+      index: ['Brand', 'Description'],
+
+      // List of keys to store and make available in your UI. The values of
+      // the keys are taken from the normalizer function below.
+      // Default: all fields
+      store: ['Brand', 'Name'],
+
+      // Function used to map the result from the GraphQL query. This should
+      // return an array of items to index in the form of flat objects
+      // containing properties to index. The objects must contain the `ref`
+      // field above (default: 'id'). This is required.
+      normalizer: ({ data }) =>
+        data.allDataJson.nodes.map((node) => ({
+          Brand: node.Brand,
+          Name: node.Name,
+          Description: node.Description,
+        })),
+    },
+  },
 ]
 };
 
